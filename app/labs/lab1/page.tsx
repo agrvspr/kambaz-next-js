6c8e22ddbe5b6bd2d263bdd50d5de6aaa1d906c6
Daniel Jea <daniel.jea786@gmail.com>
2026-01-19 18:31:32 -0500

    1.3.10

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -91,7 +91,54 @@ tag to tell browsers to render the gaps.
               <td>2/10/21</td>
               <td>90</td>
             </tr>
-            <tr> ... </tr>
+            <tr>
+              <td>Q3</td>
+              <td>HTML</td>
+              <td>2/11/22</td>
+              <td>60</td>
+            </tr>
+            <tr>
+              <td>Q4</td>
+              <td>HTML</td>
+              <td>2/12/23</td>
+              <td>61</td>
+            </tr>
+            <tr>
+              <td>Q5</td>
+              <td>HTML</td>
+              <td>2/13/24</td>
+              <td>862</td>
+            </tr>
+            <tr>
+              <td>Q6</td>
+              <td>HTML</td>
+              <td>2/14/25</td>
+              <td>63</td>
+            </tr>
+            <tr>
+              <td>Q7</td>
+              <td>HTML</td>
+              <td>2/15/26</td>
+              <td>64</td>
+            </tr>
+            <tr>
+              <td>Q8</td>
+              <td>HTML</td>
+              <td>2/16/27</td>
+              <td>65</td>
+            </tr>
+            <tr>
+              <td>Q9</td>
+              <td>HTML</td>
+              <td>2/17/28</td>
+              <td>66</td>
+            </tr>
+            <tr>
+              <td>10</td>
+              <td>HTML</td>
+              <td>2/18/29</td>
+              <td>67</td>
+            </tr>
           </tbody>
           <tfoot>
             <tr>
@@ -101,6 +148,108 @@ tag to tell browsers to render the gaps.
           </tfoot>
         </table>
       </div>
-      </div>
+      <div id="wd-images">
+        <h4>Image tag</h4>
+        Loading an image from the internet: <br />
+        <img id="wd-starship" width="400px"   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+        <br />
+        Loading a local image:
+        <br />
+        <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" />
+        </div>
+        <div id="wd-forms">
+          <h4>Form Elements</h4>
+          <form id="wd-text-fields">
+            <h5>Text Fields</h5>
+            <label htmlFor="wd-text-fields-username">Username:</label>
+            <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+            <label htmlFor="wd-text-fields-password">Password:</label>
+            <input type="password" defaultValue="123@#$asd"
+            id="wd-text-fields-password" /><br />
+            <label htmlFor="wd-text-fields-first-name">First name:</label>
+            <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+            <label htmlFor="wd-text-fields-last-name">Last name:</label>
+            <input type="text" placeholder="Doe"
+            defaultValue="Wonderland"
+            title="The last name"
+            id="wd-text-fields-last-name" />
+            </form>
+        </div>
+        <h5>Text boxes</h5>
+        <label>Biography:</label><br/>
+        <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+      <h5 id="wd-radio-buttons">Radio buttons</h5>
+      <label>Favorite movie genre:</label><br />
+      <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+      <label htmlFor="wd-radio-comedy">Comedy</label><br />
+      <input type="radio" name="radio-genre" id="wd-radio-drama"/>
+      <label htmlFor="wd-radio-drama">Drama</label><br />
+      <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+      <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+      <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+      <label htmlFor="wd-radio-fantasy">Fantasy</label>
+      <h5 id="wd-checkboxes">Checkboxes</h5>
+      <label>Favorite movie genre:</label><br/>
+      <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+      <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+      <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+      <label htmlFor="wd-chkbox-drama">Drama</label><br/>
+      <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+      <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+      <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+      <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+
+      <h4 id="wd-dropdowns">Dropdowns</h4>
+
+<h5>Select one</h5>
+<label  htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+<select id="wd-select-one-genre">
+   <option value="COMEDY">Comedy</option>
+   <option value="DRAMA">Drama</option>
+   <option selected value="SCIFI">
+       Science Fiction</option>
+   <option value="FANTASY">Fantasy</option>
+</select>
+
+<h5>Select many</h5>
+<label  htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+<select multiple id="wd-select-many-genre">
+   <option value="COMEDY" selected> Comedy          </option>
+   <option value="DRAMA">           Drama           </option>
+   <option value="SCIFI"  selected> Science Fiction </option>
+   <option value="FANTASY">         Fantasy         </option>
+</select>
+
+<h4>Other HTML field types</h4>
+
+<label htmlFor="wd-text-fields-email"> Email: </label>
+<input type="email"
+       placeholder="jdoe@somewhere.com"
+       id="wd-text-fields-email"/><br/>
+
+<label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+<input type="number"
+       defaultValue="100000"
+       placeholder="1000"
+       id="wd-text-fields-salary-start"/><br/>
+
+<label htmlFor="wd-text-fields-rating"> Rating: </label>
+<input type="range"
+       defaultValue="4"
+       max="5"
+       placeholder="Doe"
+       id="wd-text-fields-rating"/><br/>
+
+<label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+<input type="date"
+       defaultValue="2000-01-21"
+       id="wd-text-fields-dob"/><br/>
+
+<h4>Anchor tag</h4>
+Please
+<a href="https://github.com/agrvspr/kambaz-next-js" id="wd-github"> click here </a>
+to go to git<br/>
+
+</div>
 
 );}
