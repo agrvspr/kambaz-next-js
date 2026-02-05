a3ac1438f9e888c2499bf970dca3dbab9d969707
Daniel Jea <daniel.jea786@gmail.com>
2026-02-05 09:22:27 -0500

    fixed a1

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -16,9 +16,29 @@ export default function Modules() {
           </ul>
         </li>
         <li className="wd-module">
-          <div className="wd-title">Week 2</div> </li>
+          <div className="wd-title">Week 2</div> 
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Know what I am doing T_T</li>
+                <li className="wd-content-item">Realize Html is kinda annoying</li>
+              </ul>
+            </li>
+          </ul>
+        </li>  
         <li className="wd-module">
-          <div className="wd-title">Week 3</div> </li>
+          <div className="wd-title">Week 3</div> 
+          <ul className="wd-lessons">
+            <li className="wd-lesson">
+              <span className="wd-title">LEARNING OBJECTIVES</span>
+              <ul className="wd-content">
+                <li className="wd-content-item">Cry weep shiver</li>
+                <li className="wd-content-item">realize that I used the wrong repo in vercel T_T</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
       </ul>
     </div>
 );}
--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
           <Link href="/courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/reactjs.jpg" width={200} height={150} alt="reactjs" />
+            <Image src="/images/webdev.jpeg" width={200} height={150} alt="webdev" />
             <div>
               <h5> CS1234 React JS </h5>
               <p className="wd-dashboard-course-title">
@@ -18,8 +18,42 @@ export default function Dashboard() {
             </div>
           </Link>
         </div>
-        <div className="wd-dashboard-course"> ... </div>
-        <div className="wd-dashboard-course"> ... </div>
+        <div className="wd-dashboard-course"> 
+          <Link href="/courses/3650" className="wd-dashboard-course-link">
+            <Image src="/images/systems.jpeg" width={200} height={150} alt="systems" />
+            <div>
+              <h5> CS3650 </h5>
+              <p className="wd-dashboard-course-title">
+                Computer Systems
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">  
+          <Link href="/courses/3650" className="wd-dashboard-course-link">
+            <Image src="/images/math.jpeg" width={200} height={150} alt="Algo" />
+            <div>
+              <h5> CS3000 </h5>
+              <p className="wd-dashboard-course-title">
+                Algorithms and Data
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course"> 
+          <Link href="/courses/2220" className="wd-dashboard-course-link">
+            <Image src="/images/hist.jpeg" width={200} height={150} alt="hist" />
+            <div>
+              <h5> Hist2220</h5>
+              <p className="wd-dashboard-course-title">
+                History of technology
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
 );}
