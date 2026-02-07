1c7be8cf41d6aa9276801e596393dfb523f47a53
Daniel Jea <daniel.jea786@gmail.com>
2026-02-06 23:55:05 -0500

    at 2.5.5

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -1,44 +1,39 @@
+import ModulesControls from "./modulesControls";
+import { ListGroup } from "react-bootstrap";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+import LessonControlButtons from "./LessonControlButtons";
+import ModuleControlButtons from "./ModuleControlButtons";
+import { IoEllipsisVertical } from "react-icons/io5";
+
 export default function Modules() {
   return (
     <div>
-      {/* Implement Collapse All button, View Progress button, etc. */}
-      <ul id="wd-modules">
-        <li className="wd-module">
-          <div className="wd-title">Week 1</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">Learn what is Web Development</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 2</div> 
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Know what I am doing T_T</li>
-                <li className="wd-content-item">Realize Html is kinda annoying</li>
-              </ul>
-            </li>
-          </ul>
-        </li>  
-        <li className="wd-module">
-          <div className="wd-title">Week 3</div> 
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Cry weep shiver</li>
-                <li className="wd-content-item">realize that I used the wrong repo in vercel T_T</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-      </ul>
+  <ModulesControls /><br /><br /><br /><br />
+  <ListGroup className="rounded-0" id="wd-modules">
+    <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
+      <div className="wd-title p-3 ps-2 bg-secondary">
+        <IoEllipsisVertical className="me-2 fs-3" /> Week 1 <ModuleControlButtons />
+      </div>
+      <ListGroup className="wd-lessons rounded-0">
+        <ListGroupItem className="wd-lesson p-3 ps-1">
+          <IoEllipsisVertical className="me-2 fs-3" /> LEARNING OBJECTIVES <LessonControlButtons />
+          </ListGroupItem>
+        <ListGroupItem className="wd-lesson p-3 ps-1">
+          <IoEllipsisVertical className="me-2 fs-3" /> Introduction to the course <LessonControlButtons /> 
+          </ListGroupItem>
+        <ListGroupItem className="wd-lesson p-3 ps-1">
+          Learn what is Web Development </ListGroupItem>
+      </ListGroup>
+    </ListGroupItem>
+    <ListGroupItem className="wd-module p-0 mb-5 fs-5 border-gray">
+      <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 </div>
+      <ListGroup className="wd-lessons rounded-0">
+        <ListGroupItem className="wd-lesson p-3 ps-1">
+          LESSON 1 </ListGroupItem>
+        <ListGroupItem className="wd-lesson p-3 ps-1">
+          LESSON 2 </ListGroupItem>
+      </ListGroup>
+    </ListGroupItem>
+  </ListGroup>
     </div>
 );}
--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -1,59 +1,120 @@
 import Link from "next/link";
 import Image from "next/image";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CardBody from "react-bootstrap/CardBody";
+import CardTitle from "react-bootstrap/CardTitle";
+import Card from "react-bootstrap/Card";
+import CardText from "react-bootstrap/CardText";
+import CardImg from "react-bootstrap/CardImg";
+import Button from "react-bootstrap/Button";
 export default function Dashboard() {
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12) Should we change this to 7?</h2> <hr />
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link href="/courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/webdev.jpeg" width={200} height={150} alt="webdev" />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course"> 
-          <Link href="/courses/3650" className="wd-dashboard-course-link">
-            <Image src="/images/systems.jpeg" width={200} height={150} alt="systems" />
-            <div>
-              <h5> CS3650 </h5>
-              <p className="wd-dashboard-course-title">
-                Computer Systems
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">  
-          <Link href="/courses/3650" className="wd-dashboard-course-link">
-            <Image src="/images/math.jpeg" width={200} height={150} alt="Algo" />
-            <div>
-              <h5> CS3000 </h5>
-              <p className="wd-dashboard-course-title">
-                Algorithms and Data
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course"> 
-          <Link href="/courses/2220" className="wd-dashboard-course-link">
-            <Image src="/images/hist.jpeg" width={200} height={150} alt="hist" />
-            <div>
-              <h5> Hist2220</h5>
-              <p className="wd-dashboard-course-title">
-                History of technology
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
+        <Row xs={1} md={5} className="g-4">
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/1234/home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/reactjs.webp" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1234 React JS</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  Full Stack software developer</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
+            <Card>
+              <Link href="/courses/3650/home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/systems.jpeg" width="100%" height={160} />
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS3650</CardTitle>
+                <CardText className="wd-dashboard-course-description overflow-hidden" style={{height: "100px "}}>
+                  Computer Systems </CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
+            <Card>
+              <Link href="/courses/3000/home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/math.jpeg" width="100%" height={160} />
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">3000</CardTitle>
+                <CardText className="wd-dashboard-course-description overflow-hidden" style={{height: "100px "}}>
+                  Algorithms and Data </CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
+            <Card>
+              <Link href="/courses/2220/home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/hist.jpeg" width="100%" height={160} />
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">HIST2220</CardTitle>
+                <CardText className="wd-dashboard-course-description overflow-hidden" style={{height: "100px "}}>
+                  History of Technology </CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
+            <Card>
+              <Link href="/courses/1210/home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/stacked.jpg" width="100%" height={160} />
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">1210</CardTitle>
+                <CardText className="wd-dashboard-course-description overflow-hidden" style={{height: "100px "}}>
+                  Professional Development Co-op </CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
+            <Card>
+              <Link href="/courses/2550/home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/angel-falls.jpeg" width="100%" height={160} />
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CY2550</CardTitle>
+                <CardText className="wd-dashboard-course-description overflow-hidden" style={{height: "100px "}}>
+                  Cybersecurity </CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}> 
+            <Card>
+              <Link href="/courses/2800/home"
+                  className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/lc.jpeg" width="100%" height={160} />
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">2800</CardTitle>
+                <CardText className="wd-dashboard-course-description overflow-hidden" style={{height: "100px "}}>
+                  Logic and Computation </CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+        </Row>
+        
       </div>
     </div>
 );}
