13ef2cd567c5fe55bd995b2db0fe5cb955b11fdb
Daniel Jea <daniel.jea786@gmail.com>
2026-03-04 12:05:27 -0500

    4.4.1

---
+++ app/labs/lab4/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+import Link from "next/link";
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ChildStateComponent from "./ChildStateComponent";
+import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariables";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import UrlEncoding from "./query-parameters";
+import StringStateVariables from "./StringStateVariables";
+import store from "./store";
+import { Provider } from "react-redux";
+
+export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <Provider store={store}>
+    <div id="wd-passing-functions">
+      <h2>Lab 4</h2>
+      <Link href="lab4/redux">Redux Examples</Link>
+
+      <ClickEvent/>
+      <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello} />
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
+      <UrlEncoding/>
+    </div>
+    </Provider>
+);}
---
+++ app/labs/lab4/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import helloReducer from "./redux/hello/helloReducer";
+import addReducer     from "./redux/AddRedux/addReducer";
+import counterReducer from "./redux/CounterRedux/counterReducer";
+import todosReducer from "./redux/todos/todosReducer";
+const store = configureStore({
+  reducer: { helloReducer,
+    counterReducer,
+    addReducer,
+    todosReducer,
+   }});
+export type RootState = ReturnType<typeof store.getState>;
+export default store;
