80d152189d86f6d521a1c757f19a3d90e1bc8106
Daniel Jea <daniel.jea786@gmail.com>
2026-03-13 14:30:36 -0400

    Vercel fix v2

--- app/labs/lab4/url-encoding/query-params/page.tsx
+++ app/labs/lab4/url-encoding/query-params/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 import { Suspense } from "react";
-
+export const dynamic = "force-dynamic"; //THE APPARANT FIX FOR VERCEL
 import { useSearchParams } from "next/navigation";
 
 
 export default function QueryCalculator() {
  const searchParams = useSearchParams();
-
-
  const aRaw = searchParams.get("a") || "0";
  const bRaw = searchParams.get("b") || "0";
-
-
  const a = parseFloat(aRaw);
  const b = parseFloat(bRaw);
  const sum = a + b;
@@ -20,8 +16,6 @@ export default function QueryCalculator() {
  return (
    <div style={{ padding: 40 }}>
      <h1>Calculator – Query Parameters</h1>
-
-
      Raw query values (already decoded by Next.js):
      <p>
        a = <code>{aRaw}</code>
@@ -29,8 +23,6 @@ export default function QueryCalculator() {
      <p>
        b = <code>{bRaw}</code>
      </p>
-
-
      <h2 style={{ color: "green" }}>Sum = {sum}</h2>
    </div>
  );
