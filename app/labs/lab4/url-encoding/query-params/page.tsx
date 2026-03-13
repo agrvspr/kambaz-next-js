49368ed0be21b3ca8887865b3f2f50483858bc62
Daniel Jea <daniel.jea786@gmail.com>
2026-03-13 14:33:51 -0400

    update querycalc to work for suspense

--- app/labs/lab4/url-encoding/query-params/page.tsx
+++ app/labs/lab4/url-encoding/query-params/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { Suspense } from "react";
-export const dynamic = "force-dynamic"; //THE APPARANT FIX FOR VERCEL
 import { useSearchParams } from "next/navigation";
 
 
-export default function QueryCalculator() {
+function QueryCalculatorInner() {
  const searchParams = useSearchParams();
  const aRaw = searchParams.get("a") || "0";
  const bRaw = searchParams.get("b") || "0";
@@ -26,4 +25,12 @@ export default function QueryCalculator() {
      <h2 style={{ color: "green" }}>Sum = {sum}</h2>
    </div>
  );
+}
+
+export default function QueryCalculator() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <QueryCalculatorInner />
+    </Suspense>
+  );
 }
