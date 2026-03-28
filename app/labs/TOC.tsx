21de96311d022d5173a28a6577d9cdf4388da19b
Daniel Jea <daniel.jea786@gmail.com>
2026-03-27 22:56:39 -0400

    5.2.43 on

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -22,7 +22,10 @@ export default function TOC() {
         <NavLink href="/labs/lab4" as={Link} className={`nav-link ${pathname.endsWith("lab4") ? "active" : ""}`}>
          Lab 4 </NavLink> </NavItem>
      <NavItem>
-       <NavLink href="/" as={Link}>
+        <NavLink href="/labs/lab5" as={Link} className={`nav-link ${pathname.endsWith("lab5") ? "active" : ""}`}>
+         Lab 5 </NavLink> </NavItem>
+     <NavItem>
+       <NavLink href="/" as={Link}> 
          Kambaz </NavLink> </NavItem>
      <NavItem>
        <NavLink href="https://github.com/agrvspr/kambaz-next-js">My GitHub Repo Link</NavLink></NavItem>
