c619984e7ffd24c84d4b3afdc26790d1ac72a465
Daniel Jea <daniel.jea786@gmail.com>
2026-04-11 21:13:25 -0400

    yes

--- app/(kambaz)/account/Navigation.tsx
+++ app/(kambaz)/account/Navigation.tsx
@@ -7,13 +7,17 @@ import { RootState } from "../store";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: RootState) => state.accountReducer);
   const links = currentUser ? ["profile"] : ["signin", "signup"];
- const pathname = usePathname();
- return (
-   <Nav variant="pills">
-     {links.map((link) => (
-       <NavItem key={link}>
-         <NavLink as={Link} href={link} active={pathname.endsWith(link)}>
-           {link} </NavLink> </NavItem>
-     ))}
-   </Nav>
-);}
+  const pathname = usePathname();
+  return (
+    <Nav variant="pills">
+      {links.map((link) => (
+        <NavItem key={link}>
+          <NavLink as={Link} href={link} active={pathname.endsWith(link)}>
+            {link} </NavLink> </NavItem>
+
+      ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <NavLink as={Link} href={`/account/users`} active={pathname.endsWith('Users')}> Users </NavLink>)}
+    </Nav>
+  );
+}
